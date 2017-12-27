1ab759f7de9af01cde2b4c432473ceeb8b404c88
Andres G. Aragoneses <knocte@gmail.com>
2017-12-27 17:04:42 +0800

    First test passing (TDD: 'green' phase)

--- FsharpExchangeDotNetStandard/Library.fs
+++ FsharpExchangeDotNetStandard/Library.fs
@@ -1,5 +1,6 @@
 namespace FsharpExchangeDotNetStandard
 
+open System
 open System.Collections.Generic
 
 type public Currency =
@@ -17,22 +18,55 @@ type OrderBookSide() =
     inherit List<LimitOrder>()
 
 type OrderBook() =
+    let buySide = OrderBookSide()
+    let sellSide = OrderBookSide()
+
+    member internal x.InsertOrder (limitOrder: LimitOrder) =
+        // NO LOCKING BECAUSE Exchange.SendOrder() is assumed to hold a lock already
+        let side =
+            match limitOrder.Side with
+            | Side.Buy -> buySide
+            | Side.Sell -> sellSide
+        side.Add limitOrder
     member x.Item
         with get (side: Side) =
-            let someSide: OrderBookSide = new OrderBookSide()
-            someSide
+            match side with
+            | Side.Buy -> buySide
+            | Side.Sell -> sellSide
 
 type public Market =
     { BuyCurrency: Currency; SellCurrency: Currency }
 
-
 type public Exchange() =
     let markets = Dictionary<Market, OrderBook>()
 
+    let lockObject = Object()
+
     member x.SendOrder (limitOrder: LimitOrder, market: Market) =
-        ()
+        lock lockObject (
+            fun _ ->
+                let exists,maybeOrderBook = markets.TryGetValue market
+                let orderBook =
+                    match (exists,maybeOrderBook) with
+                    | false,_ ->
+                        let newOrderBook = OrderBook()
+                        markets.Add(market,newOrderBook)
+                        newOrderBook
+                    | true,orderBookFound ->
+                        orderBookFound
+                orderBook.InsertOrder limitOrder
+            )
 
     member x.Item
-        with get (market: Market) =
-            new OrderBook()
-        
+        with get (market: Market): OrderBook =
+            lock lockObject (
+                fun _ ->
+                    let exists,maybeOrderBook = markets.TryGetValue market
+                    let orderBook =
+                        match (exists,maybeOrderBook) with
+                        | false,_ ->
+                            OrderBook()
+                        | true,orderBookFound ->
+                            orderBookFound
+                    orderBook
+                )
