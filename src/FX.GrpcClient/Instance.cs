844b45ffa11dae9aae16846924b0d75090d90bfe
webwarrior <reg@webwarrior.ws>
2024-02-06 10:41:27 +0100

    Replace WebSocketApp proj with GRPC*
    Removed WebSocket project and added GRPC projects from RIM
    (copy&paste).
    
    Then removed unused stuff from GRPC* projects and specified
    package versions in Directory.Packages.props.

---
+++ src/FX.GrpcClient/Instance.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Grpc.Core;
+using Grpc.Net.Client;
+
+using GrpcService;
+
+namespace GrpcClient
+{
+    public class Instance
+    {
+        private static string serverFqdn =
+            "localhost";
+
+        public FXGrpcService.FXGrpcServiceClient Connect()
+        {
+            var channel = GrpcChannel.ForAddress($"http://{serverFqdn}:8080");
+            var client = new FXGrpcService.FXGrpcServiceClient(channel);
+            return client;
+        }
+
+        public async Task<string> SendMessage(string message)
+        {
+            var client = Connect();
+            var reply = await client.GenericMethodAsync(
+                new GenericInputParam { MsgIn = "hello" }
+            );
+            Console.WriteLine($"Got response: {reply.MsgOut}");
+
+            return reply.MsgOut;
+        }
+    }
+}
---
+++ src/FX.GrpcService/Services/FXService.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+using Grpc.Core;
+
+namespace GrpcService.Services
+{
+    public class FXService : FXGrpcService.FXGrpcServiceBase
+    {
+        public override async Task<GenericOutputParam> GenericMethod(GenericInputParam request, ServerCallContext context)
+        {
+            Console.WriteLine($"Received {request.MsgIn}");
+
+            return await Task.FromResult(new GenericOutputParam { MsgOut = "received " + request.MsgIn });
+        }
+
+        public override async Task GenericStreamOutputMethod(GenericInputParam request, IServerStreamWriter<GenericOutputParam> responseStream, ServerCallContext context)
+        {
+            Console.WriteLine(request.MsgIn);
+
+            await responseStream.WriteAsync(new GenericOutputParam { MsgOut = "ack" });
+        }
+    }
+}
